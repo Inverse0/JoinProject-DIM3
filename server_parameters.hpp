c3146721f2f070bb867ba8c8a012e770d8377f89
Leo Zhang <leozhang21@outlook.com>
2024-09-26 15:17:56 -0400

    update parameter

--- server_parameters.hpp
+++ server_parameters.hpp
@@ -3,9 +3,9 @@
 
 //=========↓↓↓============
 // copy your server parameters here!!
-const unsigned int __L1cache_size = 32768;
-const unsigned int __L2cache_size = 262144;
-const unsigned int __L3cache_size = 12582912; 
+const unsigned int __L1cache_size = 1153433;
+const unsigned int __L2cache_size = 31457280;
+const unsigned int __L3cache_size = 37748736;
 const unsigned int __L1cache_size_by_int = __L1cache_size / sizeof(int);
 const unsigned int __L2cache_size_by_int = __L2cache_size / sizeof(int);
 const unsigned int __L3cache_size_by_int = __L3cache_size / sizeof(int);
@@ -13,27 +13,25 @@ const unsigned int __cache_line_size = 64;
 const unsigned int __partition_para = 10000;
 
 
-const double Para_Radix_Flat_Hash_Table_cost = 38.006142; // 1e-9 seconds
-const double Para_Radix_LSMapper_build_cost = 21.381317; // 1e-9 seconds
-const double Para_Radix_LSMapper_probe_cost = 17.218456; // 1e-9 seconds
+const double Para_Radix_Flat_Hash_Table_cost = 39.630440; // 1e-9 seconds
+const double Para_Radix_LSMapper_build_cost = 20.117983; // 1e-9 seconds
+const double Para_Radix_LSMapper_probe_cost = 20.510995; // 1e-9 seconds
 
-const double Para_Rx_Dense = 1.956135; // 1e-9 seconds
-const double Para_Rx_Sparse = 4.879672; // 1e-9 seconds
-const double Para_Sz_Bitmap = 3.484022; // 1e-9 seconds
+const double Para_Rx_Dense = 2.303445; // 1e-9 seconds
+const double Para_Rx_Sparse = 3.861233; // 1e-9 seconds
+const double Para_Sz_Bitmap = 3.183517; // 1e-9 seconds
 
-const double Para_Sz_mem_seq_read = 0.121759; // 1e-9 seconds
-const double Para_Sz_mem_rand_read_1k = 0.499693; // 1e-9 seconds
-const double Para_Sz_mem_rand_read_write_1k = 0.443129; // 1e-9 seconds
-const double Para_Sz_mem_rand_read_2L2 = 1.005135; // 1e-9 seconds
-const double Para_Sz_mem_rand_read_write_2L2 = 1.324415; // 1e-9 seconds
-const double Para_Sz_mem_rand_read_05L3 = 1.552501; // 1e-9 seconds
-const double Para_Sz_mem_rand_read_write_05L3 = 1.914639; // 1e-9 seconds
-const double Para_Sz_mem_rand_read_15L3 = 3.750952; // 1e-9 seconds
-const double Para_Sz_mem_rand_read_write_15L3 = 5.359059; // 1e-9 seconds
-const double Para_Sz_mem_rand_read_3L3 = 5.417254; // 1e-9 seconds
-const double Para_Sz_mem_rand_read_write_3L3 = 7.911922; // 1e-9 seconds
-const double Para_Sz_mem_rand_read_10L3 = 7.198486; // 1e-9 seconds
-const double Para_Sz_mem_rand_read_write_10L3 = 11.245022; // 1e-9 seconds
+const double Para_Sz_mem_seq_read = 0.053652; // 1e-9 seconds
+const double Para_Sz_mem_rand_read_1k = 0.704767; // 1e-9 seconds
+const double Para_Sz_mem_rand_read_write_1k = 0.556446; // 1e-9 seconds
+const double Para_Sz_mem_rand_read_2L2 = 7.162653; // 1e-9 seconds
+const double Para_Sz_mem_rand_read_write_2L2 = 7.005525; // 1e-9 seconds
+const double Para_Sz_mem_rand_read_05L3 = 3.645304; // 1e-9 seconds
+const double Para_Sz_mem_rand_read_write_05L3 = 3.759896; // 1e-9 seconds
+const double Para_Sz_mem_rand_read_15L3 = 6.278703; // 1e-9 seconds
+const double Para_Sz_mem_rand_read_write_15L3 = 6.461476; // 1e-9 seconds
+const double Para_Sz_mem_rand_read_3L3 = 8.033285; // 1e-9 seconds
+const double Para_Sz_mem_rand_read_write_3L3 = 8.876325; // 1e-9 seconds
 //=========↑↑↑============
 
 static double cal_mem_rand_read_cost(int array_size) {
